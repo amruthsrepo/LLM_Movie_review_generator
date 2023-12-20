3371622fbb5a6a2aa2808adf8addb630867d37be
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2023-12-19 19:43:22 -0500

    v 0.1 Review generation

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 import json
 import os
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 DEFAULT_OPEN_AI_MODEL = 'text-davinci-003'
 
 app = Flask(__name__)
 CORS(app)
+
+@app.get('/api/get/review')
+@cross_origin()
+def get_review():
+    args = request.args
+    movie_title=args.get('movie_title')
+
+    prompt = "Write a movie review for the movie \"{movie_title}\". The review should be less than 100 words long. The review should be written in the first person. The review should be written in the active voice."
+    prompt = prompt.format(
+        movie_title=movie_title,
+    )
+    response = client.completions.create(model = DEFAULT_OPEN_AI_MODEL,
+    prompt = prompt,
+    temperature = 0.5,
+    max_tokens = 500,
+    top_p = 1,
+    frequency_penalty = 0.0,
+    presence_penalty=1.0)
+
+    return response.choices[0].text.strip()
+
 
 @app.get('/api/get/plot')
 @cross_origin()
@@ -24,15 +46,13 @@ def get_plot():
         movie_title=movie_title,
         plot_words=PLOT_WORDS,
     )
-    response = openai.Completion.create(
-        model=DEFAULT_OPEN_AI_MODEL,
-        prompt=PROMPT,
-        temperature=1,
-        max_tokens=PLOT_WORDS * 2,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    response = client.completions.create(model=DEFAULT_OPEN_AI_MODEL,
+    prompt=PROMPT,
+    temperature=1,
+    max_tokens=PLOT_WORDS * 2,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
     return response.choices[0].text.strip()
 
 @app.get('/api/get/director')
@@ -43,15 +63,13 @@ def get_director():
     PROMPT='Give me the director of the movie "{movie_title}", give me just the full name with no other words, characters, symbols nor punctuation added'.format(
         movie_title=movie_title
     )
-    response = openai.Completion.create(
-        model=DEFAULT_OPEN_AI_MODEL,
-        prompt=PROMPT,
-        temperature=1,
-        max_tokens=64,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    response = client.completions.create(model=DEFAULT_OPEN_AI_MODEL,
+    prompt=PROMPT,
+    temperature=1,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
     return response.choices[0].text.strip()
 
 @app.get('/api/get/year')
@@ -62,15 +80,13 @@ def get_year():
     PROMPT='Give me the year of the release of the movie "{movie_title}", give me just the year with no other words, characters, symbols nor punctuation added'.format(
         movie_title=movie_title
     )
-    response = openai.Completion.create(
-        model=DEFAULT_OPEN_AI_MODEL,
-        prompt=PROMPT,
-        temperature=1,
-        max_tokens=64,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    response = client.completions.create(model=DEFAULT_OPEN_AI_MODEL,
+    prompt=PROMPT,
+    temperature=1,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
     return response.choices[0].text.strip()
 
 @app.get('/api/get/genre')
@@ -81,15 +97,13 @@ def get_genre():
     PROMPT='Give me the genre of the movie "{movie_title}", give me just the genre with no other words, characters, symbols nor punctuation added'.format(
         movie_title=movie_title
     )
-    response = openai.Completion.create(
-        model=DEFAULT_OPEN_AI_MODEL,
-        prompt=PROMPT,
-        temperature=1,
-        max_tokens=64,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    response = client.completions.create(model=DEFAULT_OPEN_AI_MODEL,
+    prompt=PROMPT,
+    temperature=1,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0)
     return response.choices[0].text.strip()
 
 @app.route('/')
